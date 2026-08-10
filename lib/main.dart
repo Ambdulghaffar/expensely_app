a08991d10100d2d4ba2328044d6e909c447e1c8d
Ambdulghaffar <elhaffarahamadi@gmail.com>
2026-08-11 00:30:00 +0100

    feat: initialize Firebase and add firebase_auth dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:expensely_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/navigation/app_router.dart';
 import 'core/theme/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
